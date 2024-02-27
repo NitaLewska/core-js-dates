fa993db8548861bb1776277f3bb95829ffdb4d5f
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2024-02-27 15:26:15 +0300

    feat: solved 5/14

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -196,8 +196,8 @@ function getNextFridayThe13th(/* date */) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  return Math.floor(new Date(date).getMonth() / 3) + 1;
 }
 
 /**
@@ -234,8 +234,9 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
 }
 
 module.exports = {
