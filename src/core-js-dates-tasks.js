077f0bdbc8336de374bb2bd0ef7e857c549ffaef
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2024-03-12 14:26:42 +0300

    feat: solved 12/14

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -227,8 +227,24 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const month = date.getMonth();
+  let days = date.getDate();
+  for (let i = 0; i < month; i += 1) {
+    days += daysInMonths[i];
+  }
+  let firstDay = new Date(date.getFullYear(), 0, 1).getDay();
+  if (firstDay === 0) {
+    firstDay = 7;
+  }
+  let weeks;
+  if (firstDay === 1) {
+    weeks = Math.ceil(days / 7);
+  } else {
+    weeks = Math.ceil((days - (8 - firstDay)) / 7) + 1;
+  }
+  return weeks;
 }
 
 /**
